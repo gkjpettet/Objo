17a3abb528ce7b73810de1e0a0e0564afddc1cc8
Garry Pettet <contact@garrypettet.com>
2023-10-24 11:20:27 +0100

    Initial package commit

--- Sources/Objo/Objo.swift
+++ Sources/Objo/Objo.swift
@@ -1,2 +1,12 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+struct Tokeniser {
+    func echo(what: String) {
+        print(what)
+    }
+    
+    func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+}
+
