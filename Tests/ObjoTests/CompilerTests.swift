47a51b6795e832ed8e372d01c897067c1918c3e1
Garry Pettet <contact@garrypettet.com>
2023-11-10 10:06:23 +0000

    Added test for compiling boolean literals.

--- Tests/ObjoTests/CompilerTests.swift
+++ Tests/ObjoTests/CompilerTests.swift
@@ -8,20 +8,36 @@ final class CompilerTests: XCTestCase {
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        compiler.reset()
     }
     
     /// Tests parsing an empty source code input.
-    func testBasic() throws {
-//        let source = "1 + 2"
-//        
-//        do {
-//            let function = try compiler.compile(source: source, debugMode: true, scriptID: 0)
-//            var offset = 0
-//            let bytecode = try disassembler.disassembleFunction(function: function)
-//            print(bytecode)
-//            XCTAssertTrue(bytecode != "")
-//        } catch {
-//            print("\(error)")
-//        }
+    /// Expect:
+    /// ```
+    /// nothing
+    /// return_
+    /// ```
+    func testEmptySource() throws {
+        let f = try compiler.compile(source: "", debugMode: true, scriptID: 0)
+        XCTAssertTrue(f.chunk.code.count == 2)
+        XCTAssertTrue(f.chunk.code[0] == Opcode.nothing.rawValue)
+        XCTAssertTrue(f.chunk.code[1] == Opcode.return_.rawValue)
+    }
+    
+    /// Tests compiling the production of a simple boolean literal.
+    func testBooleanLiteral() throws {
+        let f1 = try compiler.compile(source: "true", debugMode: true, scriptID: 0)
+        XCTAssertTrue(f1.chunk.code.count == 4)
+        XCTAssertTrue(f1.chunk.code[0] == Opcode.true_.rawValue)
+        XCTAssertTrue(f1.chunk.code[1] == Opcode.pop.rawValue)
+        XCTAssertTrue(f1.chunk.code[2] == Opcode.nothing.rawValue)
+        XCTAssertTrue(f1.chunk.code[3] == Opcode.return_.rawValue)
+        
+        let f2 = try compiler.compile(source: "false", debugMode: true, scriptID: 0)
+        XCTAssertTrue(f2.chunk.code.count == 4)
+        XCTAssertTrue(f2.chunk.code[0] == Opcode.false_.rawValue)
+        XCTAssertTrue(f2.chunk.code[1] == Opcode.pop.rawValue)
+        XCTAssertTrue(f2.chunk.code[2] == Opcode.nothing.rawValue)
+        XCTAssertTrue(f2.chunk.code[3] == Opcode.return_.rawValue)
     }
 }
